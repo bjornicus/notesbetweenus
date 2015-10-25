a70bb543e3b8753be1b2a6bf03d7c2d0a7a4c5c3
Bjorn Hansen <bjornh@microsoft.com>
2015-10-24 20:52:54 -0700

    save on every key press

--- app/notes.js
+++ app/notes.js
@@ -1,19 +1,19 @@
 var path = location.pathname.substring(1);
 
-if (path === ''){
-	path = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
-	location.pathname = path;	
+if (path === '') {
+    path = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+    location.pathname = path;
 }
 var messageData = new Firebase('https://sweltering-fire-5125.firebaseio.com').child(path);
 var messageElement = document.getElementById('message');
 var inputElement = document.getElementById('message');
 
-messageData.on("value", function (snapshot) { 
-	messageElement.innerText = snapshot.val();
+messageData.on("value", function(snapshot) {
+    messageElement.innerText = snapshot.val();
 });
 
-inputElement.onkeypress = function (e) {
-    if (e.keyCode == 13) {
-      messageData.set(inputElement.innerText);
-    }
+inputElement.onkeypress = function(e) {
+    setTimeout(function() {
+        messageData.set(inputElement.innerText);
+    });
 }
