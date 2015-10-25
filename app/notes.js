a31b979b9d996387ce7621e6c3ea23c14c6b12c8
Bjorn Hansen <bjornh@microsoft.com>
2015-10-24 18:17:08 -0700

    set and display the message

--- app/notes.js
+++ app/notes.js
@@ -1,2 +1,13 @@
+var messageData = new Firebase('https://sweltering-fire-5125.firebaseio.com').child(location.pathname.substring(1));
+var messageElement = document.getElementById('message');
+var inputElement = document.getElementById('messageInput');
 
-alert('hi!');
+messageData.on("value", function (snapshot) { 
+	messageElement.innerText = snapshot.val();
+});
+
+inputElement.onkeypress = function (e) {
+    if (e.keyCode == 13) {
+      messageData.set(inputElement.value);
+    }
+}
