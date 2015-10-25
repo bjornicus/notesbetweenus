43d491b4eed2f36dcc21cca52266fe8755c96156
Bjorn Hansen <bjornh@microsoft.com>
2015-10-24 18:37:04 -0700

    redirect root to random 6 character path

--- app/notes.js
+++ app/notes.js
@@ -1,4 +1,10 @@
-var messageData = new Firebase('https://sweltering-fire-5125.firebaseio.com').child(location.pathname.substring(1));
+var path = location.pathname.substring(1);
+
+if (path === ''){
+	path = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 6);
+	location.pathname = path;	
+}
+var messageData = new Firebase('https://sweltering-fire-5125.firebaseio.com').child(path);
 var messageElement = document.getElementById('message');
 var inputElement = document.getElementById('messageInput');
 
